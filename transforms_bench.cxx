fd327fbdb415fe03090d236da9defb33be8385db
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-09-24 12:05:24 +0100

    Do something with the result

--- transforms_bench.cxx
+++ transforms_bench.cxx
@@ -43,10 +43,12 @@ transformVec_bench(benchmark::State& state)
                        0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
                        0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
                        0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
-      transformVec(P, S);
-      double Pcopy[45];
-      benchmark::DoNotOptimize(std::memcpy (Pcopy,P,45*sizeof(double)));
-      benchmark::DoNotOptimize(Pcopy);
+      transformVec2(P, S);
+      double sum=0;
+      for (int i=0; i<45; ++i){
+          sum+=P[i];
+      }
+      benchmark::DoNotOptimize(sum);
       benchmark::ClobberMemory();
     }
   }
@@ -67,9 +69,11 @@ transformVec2_bench(benchmark::State& state)
                        0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
                        0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
       transformVec2(P, S);
-      double Pcopy[45];
-      benchmark::DoNotOptimize(std::memcpy (Pcopy,P,45*sizeof(double)));
-      benchmark::DoNotOptimize(Pcopy);
+      double sum=0;
+      for (int i=0; i<45; ++i){
+          sum+=P[i];
+      }
+      benchmark::DoNotOptimize(sum);
       benchmark::ClobberMemory();
     }
   }
