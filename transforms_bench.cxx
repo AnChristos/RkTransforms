36af6d7ddcfe5cf0a36683012dc20160c71c408c
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-09-25 13:59:18 +0100

    do only the tranform in the bench

--- transforms_bench.cxx
+++ transforms_bench.cxx
@@ -7,17 +7,15 @@ static void
 transform_bench(benchmark::State& state)
 {
   const double S[3] = { 0.4, 0.2, 0.9 };
-  double Pstd[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-                      0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
-                      0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
-                      0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
-                      0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
+  double P[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                   0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
+                   0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
+                   0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
+                   0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
 
   for (auto _ : state) {
     const int n = state.range(0);
     for (int i = 0; i < n; ++i) {
-      double P[45];
-      std::memmove(P, Pstd, 45 * sizeof(double));
       double dummy;
       benchmark::DoNotOptimize(dummy = transform(P, S));
       benchmark::DoNotOptimize(&dummy);
@@ -31,17 +29,15 @@ static void
 transformVec_bench(benchmark::State& state)
 {
   const double S[3] = { 0.4, 0.2, 0.9 };
-  double Pstd[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-                      0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
-                      0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
-                      0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
-                      0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
+  double P[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                   0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
+                   0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
+                   0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
+                   0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
 
   for (auto _ : state) {
     const int n = state.range(0);
     for (int i = 0; i < n; ++i) {
-      double P[45];
-      std::memmove(P, Pstd, 45 * sizeof(double));
       double dummy;
       benchmark::DoNotOptimize(dummy = transformVec(P, S));
       benchmark::DoNotOptimize(&dummy);
@@ -55,17 +51,15 @@ static void
 transformVec2_bench(benchmark::State& state)
 {
   const double S[3] = { 0.4, 0.2, 0.9 };
-  double Pstd[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-                      0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
-                      0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
-                      0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
-                      0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
+  double P[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                   0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
+                   0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
+                   0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
+                   0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
 
   for (auto _ : state) {
     const int n = state.range(0);
     for (int i = 0; i < n; ++i) {
-      double P[45];
-      std::memmove(P, Pstd, 45 * sizeof(double));
       double dummy;
       benchmark::DoNotOptimize(dummy = transformVec2(P, S));
       benchmark::DoNotOptimize(&dummy);
@@ -79,17 +73,15 @@ static void
 transformVec6Transf_bench(benchmark::State& state)
 {
   const double S[3] = { 0.4, 0.2, 0.9 };
-  double Pstd[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-                      0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
-                      0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
-                      0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
-                      0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
+  double P[45] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+                   0.1,  0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18,
+                   0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
+                   0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
+                   0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
 
   for (auto _ : state) {
     const int n = state.range(0);
     for (int i = 0; i < n; ++i) {
-      double P[45];
-      std::memmove(P, Pstd, 45 * sizeof(double));
       double dummy;
       benchmark::DoNotOptimize(dummy = transformVec6Transf(P, S));
       benchmark::DoNotOptimize(&dummy);
@@ -108,12 +100,11 @@ transformVec6_bench(benchmark::State& state)
                      0.19, 0.2,  0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27,
                      0.28, 0.29, 0.3,  0.31, 0.32, 0.33, 0.34, 0.35, 0.36,
                      0.37, 0.38, 0.39, 0.4,  0.41, 0.42, 0.43, 0.44, 0.45 };
-  Pstruct6 Pstd;
-  Pstd.fromP(Pin);
+  Pstruct6 P;
+  P.fromP(Pin);
   for (auto _ : state) {
     const int n = state.range(0);
     for (int i = 0; i < n; ++i) {
-      Pstruct6 P(Pstd);
       double dummy;
       benchmark::DoNotOptimize(dummy = transformVec6(P, S));
       benchmark::DoNotOptimize(&dummy);
